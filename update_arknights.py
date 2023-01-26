017720e9e7f017d747b6b309ca4c343d6a5d7476
meeboo3 <118372959+meeboo3@users.noreply.github.com>
2023-01-27 01:44:54 +0800

    Update update_arknights.py

--- update_arknights.py
+++ update_arknights.py
@@ -1,3 +1,10 @@
+import os
+
 from resource.arknights import ArknightsResource
 
 ArknightsResource.start()
+
+if ArknightsResource.push:
+  os.system('git config --global user.email noreply@arkfans.top')
+  os.system('git config --global user.name MeeBooBot_v0')
+  os.system('git push')
