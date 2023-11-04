67e05148588b11249c0da02513c88b4b9dd7338c
meeboo3 <118372959+meeboo3@users.noreply.github.com>
2023-11-05 04:03:42 +0800

    Add files via upload

--- util/constance.py
+++ util/constance.py
@@ -2,3 +2,5 @@ static_url = 'https://static.mayertalk.top/'
 data_url = 'char/%s.json'
 special_data_url = 'char/%s.spec.json'
 version_url = 'version/char/%s.txt'
+
+lang_order = ['zh_CN', 'zh_TW', 'py', 'fpy', 'en_US', 'ja_JP']
