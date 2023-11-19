272e531225851aa08191235439f7317b9b839de2
meeboo3 <118372959+meeboo3@users.noreply.github.com>
2023-11-19 16:51:56 +0800

    fix missing lang

--- util/constance.py
+++ util/constance.py
@@ -3,4 +3,4 @@ data_url = 'char/%s.json'
 special_data_url = 'char/%s.spec.json'
 version_url = 'version/char/%s.txt'
 
-lang_order = ['zh_CN', 'zh_TW', 'py', 'fpy', 'en_US', 'ja_JP']
+lang_order = ['zh_CN', 'zh_TW', 'py', 'fpy', 'en_US', 'ja_JP', 'code']
