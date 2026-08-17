46eab696b143724776ba01a2e1ee9e84556b1747
single-ptilopsis <northwind4778@gmail.com>
2026-08-17 20:44:10 +0800

    chore: 禁用npc源

--- update_arknights.py
+++ update_arknights.py
@@ -2,4 +2,4 @@ from resources.arknights import ArknightsResource
 from resources.arknights_npc import ArknightsNPCResource
 
 ArknightsResource.start()
-ArknightsNPCResource.start()
+# ArknightsNPCResource.start()
